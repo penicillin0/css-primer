4dd1ca2a06d972ced44638ec73605c4e1b79e8ae
penicillin0 <ayahitonakamura@gmail.com>
2021-07-28 03:16:37 +0900

    simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { About } from "./pages/About/About";
 
 function App() {
-  return <div>Hello, world!</div>;
+  return (
+    <Router>
+      <Route path="/about" render={() => <About />} />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export const About: React.FC<Props> = () => {
+  return <div>This is about page</div>;
+};
