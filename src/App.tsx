0ae481158299a9e4042793d09b67b9f14dff1aab
penicillin0 <ayahitonakamura@gmail.com>
2021-07-28 07:50:01 +0900

    GitHub Pagesだと404エラーになる問題を解消

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { About } from "./pages/About/About";
 
 function App() {
+  const ROUTER_BASENAME =
+    process.env.NODE_ENV === "development" ? "/" : "/css-primer";
+
   return (
-    <Router>
-      <Route path="/about" render={() => <About />} />
+    <Router basename={ROUTER_BASENAME}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Switch>
+        <Route exact path="/about" component={About} />
+      </Switch>
     </Router>
   );
 }
