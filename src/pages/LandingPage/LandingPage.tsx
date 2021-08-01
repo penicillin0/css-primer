7c727e3cbfad31c2d38bea1946e22e23fd061190
penicillin0 <ayahitonakamura@gmail.com>
2021-08-01 17:43:52 +0900

    code block

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { monokai } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 type Props = {};
+
+const cssCode = `span:before {
+             content: "Hello"
+}
+span:after {
+            content: "World"
+}`;
 
 export const LandingPage: React.FC<Props> = () => {
   return (
     <div>
       <h1 className="my-4">Welcome to CSS primer</h1>
+      <SyntaxHighlighter language="css" style={monokai}>
+        {cssCode}
+      </SyntaxHighlighter>
       <div className="mx-4">
         <h2>Pages</h2>
         <li>
